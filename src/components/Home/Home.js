69305e45e30600d1e938374de347534344c042d6
Alejandro Santiago <cubanfireball81@gmail.com>
2022-01-26 17:50:24 -0900

    Setup of Home, About, Projects and Assets Componet files.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Hello, welcome to my page!</h1>
+        </div>
+    );
+}
+
+export default Home;
